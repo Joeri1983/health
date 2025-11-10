46075362e95a4f38e1f4f16173c7a95e425257ac
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2025-11-10 15:59:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const https = require('https');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 
-const azureStorageUrl = 'https://storagejoeri.blob.core.windows.net/dgjoeri/waardes.csv';
+const azureStorageUrl = 'https://storagejoeri.blob.core.windows.net/dgjoeri/activity.csv';
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
